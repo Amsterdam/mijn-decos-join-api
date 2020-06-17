01db7b3b6dd9904d5b3369f071ca1c4228c0a791
Johan Otten <drakonen@drakonen.com>
2020-06-17 15:49:16 +0200

    Simple script

---
+++ decosjoin/scripts/get_vergunning.py
@@ -0,0 +1,12 @@
+from sys import argv
+
+from decosjoin.api.decosjoin.decosjoin_connection import DecosJoinConnection
+from decosjoin.config import get_decosjoin_username, get_decosjoin_password, get_decosjoin_api_host, \
+    get_decosjoin_adres_boeken
+
+bsn = argv[1]
+
+connection = DecosJoinConnection(
+    get_decosjoin_username(), get_decosjoin_password(), get_decosjoin_api_host(), get_decosjoin_adres_boeken())
+
+zaken = connection.get_zaken(bsn)
