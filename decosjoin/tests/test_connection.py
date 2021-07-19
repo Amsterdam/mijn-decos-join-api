9671d81bb9df49a538d61902a3e33afe54d3e697
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2021-07-19 13:37:04 +0200

    Mijn 3437 decosjoin updates (#35)

--- decosjoin/tests/test_connection.py
+++ decosjoin/tests/test_connection.py
@@ -2,28 +2,52 @@ from unittest import TestCase
 from unittest.mock import patch
 
 from decosjoin.api.decosjoin.decosjoin_connection import DecosJoinConnection
-from decosjoin.tests.fixtures.response_mock import (get_response_mock,
-                                                    post_response_mock)
+from decosjoin.tests.fixtures.response_mock import get_response_mock, post_response_mock
 from freezegun import freeze_time
 
 
-@patch("decosjoin.crypto.get_encrytion_key", lambda: "z4QXWk3bjwFST2HRRVidnn7Se8VFCaHscK39JfODzNs=")
-@patch('decosjoin.tests.test_connection.DecosJoinConnection.get_response', get_response_mock)
+@patch(
+    "decosjoin.crypto.get_encrytion_key",
+    lambda: "z4QXWk3bjwFST2HRRVidnn7Se8VFCaHscK39JfODzNs=",
+)
+@patch(
+    "decosjoin.tests.test_connection.DecosJoinConnection.get_response",
+    get_response_mock,
+)
 @patch("decosjoin.server.DecosJoinConnection.post_response", post_response_mock)
 @freeze_time("2021-07-05")
 class ConnectionTests(TestCase):
-
     def setUp(self) -> None:
-        self.connection = DecosJoinConnection('username', 'password', 'http://localhost', {'bsn': ['hexkey32chars000000000000000BSN1', 'hexkey32chars000000000000000BSN2']})
+        self.connection = DecosJoinConnection(
+            "username",
+            "password",
+            "http://localhost",
+            {
+                "bsn": [
+                    "hexkey32chars000000000000000BSN1",
+                    "hexkey32chars000000000000000BSN2",
+                ]
+            },
+        )
 
     def test_get_user_key(self):
         user_key = self.connection.get_user_keys("bsn", "111222333")
-        self.assertEqual(user_key, ['32charsstringxxxxxxxxxxxxxxxxxxx', '32charsstringxxxxxxxxxxxxxxxxxx2', '32charsstringxxxxxxxxxxxxxxxxxx3'])
+        self.assertEqual(
+            user_key,
+            [
+                "32charsstringxxxxxxxxxxxxxxxxxxx",
+                "32charsstringxxxxxxxxxxxxxxxxxx2",
+                "32charsstringxxxxxxxxxxxxxxxxxx3",
+            ],
+        )
 
     def assert_unknown_identifier(self, zaken, identifier):
-        self.assertEqual([zaak['identifier'] for zaak in zaken if zaak['identifier'] == identifier], [])
+        self.assertEqual(
+            [zaak["identifier"] for zaak in zaken if zaak["identifier"] == identifier],
+            [],
+        )
 
-    @patch('decosjoin.api.decosjoin.decosjoin_connection.PAGE_SIZE', 10)
+    @patch("decosjoin.api.decosjoin.decosjoin_connection.PAGE_SIZE", 10)
     def test_get_zaken(self):
         zaken = self.connection.get_zaken("bsn", "111222333")
 
@@ -31,64 +55,140 @@ class ConnectionTests(TestCase):
         zaken_from_fixtures = []
 
         for z in zaken:
-            zaken_from_fixtures.append([z['identifier'], z['status'], z['decision'], z['caseType']])
+            zaken_from_fixtures.append(
+                [z["identifier"], z["status"], z["decision"], z["caseType"]]
+            )
 
         zaken_expected = [
-            ['Z/21/99012350', 'Ontvangen', 'Verleend', 'Parkeerontheffingen Blauwe zone particulieren'],
-            ['Z/21/99012349', 'Ontvangen', 'Verleend', 'Parkeerontheffingen Blauwe zone bedrijven'],
-            ['Z/21/99012348', 'Ontvangen', None, 'Omzettingsvergunning'],
-            ['Z/21/99012347', 'Ontvangen', None, 'GPK'],
-            ['Z/21/99012346', 'Ontvangen', None, 'GPP'],
-            ['Z/21/99012345', 'Ontvangen', None, 'E-RVV - TVM'],
-            ['Z/21/89012345', 'Ontvangen', None, 'Vakantieverhuur'],
-            ['Z/21/78901234', 'Ontvangen', None, 'B&B - vergunning'],
-            ['Z/21/7865356778', 'Afgehandeld', 'Verleend', 'Vakantieverhuur vergunningsaanvraag'],
-            ['Z/21/67890123', 'Ontvangen', None, 'Vakantieverhuur'],
-            ['Z/21/123123123', 'Afgehandeld', 'Ingetrokken', 'Vakantieverhuur vergunningsaanvraag'],
-            ['Z/20/9', 'Ontvangen', 'Verleend', 'TVM - RVV - Object'],
-            ['Z/20/2345678.6', 'Ontvangen', None, 'TVM - RVV - Object'],
-            ['Z/20/2345678.5', 'Ontvangen', 'Verleend', 'TVM - RVV - Object'],
-            ['Z/20/2345678.4', 'Ontvangen', None, 'TVM - RVV - Object'],
-            ['Z/20/2345678.3', 'Ontvangen', None, 'TVM - RVV - Object'],
-            ['Z/20/2345678.2', 'Ontvangen', None, 'TVM - RVV - Object'],
-            ['Z/20/2345678.1', 'Ontvangen', None, 'TVM - RVV - Object'],
-            ['Z/20/2345678.0', 'Ontvangen', None, 'TVM - RVV - Object'],
-            ['Z/20/1234567', 'Ontvangen', None, 'TVM - RVV - Object']
+            [
+                "Z/21/99012350",
+                "Ontvangen",
+                "Verleend",
+                "Parkeerontheffingen Blauwe zone particulieren",
+            ],
+            [
+                "Z/21/99012349",
+                "Ontvangen",
+                "Verleend",
+                "Parkeerontheffingen Blauwe zone bedrijven",
+            ],
+            ["Z/21/99012348", "Ontvangen", None, "Omzettingsvergunning"],
+            ["Z/21/99012347", "Ontvangen", None, "GPK"],
+            ["Z/21/99012346", "Ontvangen", None, "GPP"],
+            ["Z/21/99012345", "Ontvangen", None, "E-RVV - TVM"],
+            ["Z/21/89012345", "Ontvangen", None, "Vakantieverhuur afmelding"],
+            ["Z/21/78901234", "Ontvangen", None, "B&B - vergunning"],
+            [
+                "Z/21/7865356778",
+                "Afgehandeld",
+                "Verleend",
+                "Vakantieverhuur vergunningsaanvraag",
+            ],
+            ["Z/21/67890123", "Ontvangen", None, "Vakantieverhuur"],
+            [
+                "Z/21/123123123",
+                "Afgehandeld",
+                "Ingetrokken",
+                "Vakantieverhuur vergunningsaanvraag",
+            ],
+            ["Z/20/9", "Ontvangen", "Verleend", "TVM - RVV - Object"],
+            ["Z/20/2345678.6", "Ontvangen", None, "TVM - RVV - Object"],
+            ["Z/20/2345678.5", "Ontvangen", "Verleend", "TVM - RVV - Object"],
+            ["Z/20/2345678.4", "Ontvangen", None, "TVM - RVV - Object"],
+            ["Z/20/2345678.3", "Ontvangen", None, "TVM - RVV - Object"],
+            ["Z/20/2345678.2", "Ontvangen", None, "TVM - RVV - Object"],
+            ["Z/20/2345678.1", "Ontvangen", None, "TVM - RVV - Object"],
+            ["Z/20/2345678.0", "Ontvangen", None, "TVM - RVV - Object"],
+            ["Z/20/1234567", "Ontvangen", None, "TVM - RVV - Object"],
         ]
 
         self.assertEqual(zaken_from_fixtures, zaken_expected)
 
         # Z/21/90123456 "vakantieverhuur" is filtered out because it is replaced by Z/21/89012345 "vakantieverhuur afmelding"
-        self.assert_unknown_identifier(zaken, 'Z/21/90123456')
+        self.assert_unknown_identifier(zaken, "Z/21/90123456")
 
         # Z/20/4567890 is filtered out because of subject1 contents
-        self.assert_unknown_identifier(zaken, 'Z/20/4567890')
+        self.assert_unknown_identifier(zaken, "Z/20/4567890")
 
         # Z/20/56789012 is filtered out because of subject1 starts with "*verwijder"
-        self.assert_unknown_identifier(zaken, 'Z/20/56789012')
+        self.assert_unknown_identifier(zaken, "Z/20/56789012")
 
         # Z/20/2 is filtered out because of decision "Buiten behandeling"
-        self.assert_unknown_identifier(zaken, 'Z/20/2')
+        self.assert_unknown_identifier(zaken, "Z/20/2")
 
-    @patch('decosjoin.api.decosjoin.decosjoin_connection.PAGE_SIZE', 10)
+    @patch("decosjoin.api.decosjoin.decosjoin_connection.PAGE_SIZE", 10)
     def test_get_documents(self):
-        documents = self.connection.get_documents('ZAAKKEY1', "111222333")
+        documents = self.connection.get_documents("ZAAKKEY1", "111222333")
         self.assertEqual(len(documents), 2)
-        self.assertEqual(documents[0]['sequence'], 2)
-        self.assertEqual(documents[1]['sequence'], 5)
+        self.assertEqual(documents[0]["sequence"], 2)
+        self.assertEqual(documents[1]["sequence"], 5)
 
         doc0 = documents[0]
-        self.assertEqual(doc0['sequence'], 2)
-        self.assertEqual(doc0['id'], 'D/2')
-        self.assertTrue(doc0['url'].startswith('/api/decosjoin/document/'))
+        self.assertEqual(doc0["sequence"], 2)
+        self.assertEqual(doc0["id"], "D/2")
+        self.assertTrue(doc0["url"].startswith("/api/decosjoin/document/"))
 
         # check exclusions
-        sequence_numbers = [d['sequence'] for d in documents]
+        sequence_numbers = [d["sequence"] for d in documents]
         self.assertEqual([2, 5], sequence_numbers)
 
         self.assertNotIn(7, sequence_numbers)
         self.assertNotIn(8, sequence_numbers)
         self.assertNotIn(9, sequence_numbers)
+
+    def test_transform(self):
+        def wrap(zaak, key):
+            return {
+                "key": key,
+                "fields": zaak,
+                "links": [
+                    {
+                        "href": """https://decosdvl.acc.amsterdam.nl/decosweb/aspx/api/v1/items/{key}""",
+                        "rel": "self",
+                    }
+                ],
+            }
+
+        zaak_vakantieverhuur = {
+            "date6": "2021-06-18T00:00:00",
+            "date7": "2021-06-21T00:00:00",
+            "document_date": "2021-04-28T00:00:00",
+            "mark": "Z/21/67890123",
+            "subject1": "Melding Amstel 1  - V Achternaam",
+            "text11": "Nvt",
+            "text12": "Geen kosten",
+            "text45": "Vakantieverhuur",
+            "text6": "Amstel 1 1012AA Amsterdam",
+            "text7": "Z/11/123456",
+            "title": "Ontvangen",
+        }
+
+        zaak_vakantieverhuur_afmelding = {
+            "company": "Moes",
+            "date6": "2021-06-18T00:00:00",
+            "date7": "2021-06-21T00:00:00",
+            "document_date": "2021-05-10T00:00:00",
+            "mark": "Z/21/89012345",
+            "subject1": "Melding Amstel 1  - V Achternaam",
+            "text11": "Nvt",
+            "text45": "Vakantieverhuur afmelding",
+            "text6": "Amstel 1 1012AA Amsterdam",
+            "title": "Ontvangen",
+        }
+
+        zaken_result = self.connection.transform(
+            [
+                wrap(zaak_vakantieverhuur, "zaak1"),
+                wrap(zaak_vakantieverhuur_afmelding, "zaak2"),
+            ],
+            "test-user-id",
+        )
+
+        self.assertEqual(len(zaken_result), 1)
+        self.assertEqual(zaken_result[0]["caseType"], "Vakantieverhuur afmelding")
+        self.assertEqual(zaken_result[0]["identifier"], "Z/21/89012345")
+        self.assert_unknown_identifier(zaken_result, "Z/21/67890123")
+        self.assertTrue("documentsUrl" in zaken_result[0])
 
     # def test_get_document_blob(self):
     #     documents = self.connection.get_document_blob('DOCUMENTKEY01')
--- decosjoin/tests/test_zaaktypes.py
+++ decosjoin/tests/test_zaaktypes.py
@@ -2,21 +2,43 @@ from datetime import date
 from unittest.case import TestCase
 
 from decosjoin.api.decosjoin.field_parsers import to_date
-from decosjoin.api.decosjoin.zaaktypes import (BBVergunning, BZB, BZP, TVM_RVV_Object,
-                                               VakantieVerhuurVergunning)
+from decosjoin.api.decosjoin.zaaktypes import (
+    BZB,
+    BZP,
+    BBVergunning,
+    TVM_RVV_Object,
+    VakantieVerhuur,
+    VakantieVerhuurAfmelding,
+    VakantieVerhuurVergunning,
+)
+
+from freezegun import freeze_time
 
 
 class ZaaktypesTest(TestCase):
-
     def test_next_april_first(self):
-        self.assertEqual(VakantieVerhuurVergunning.next_april_first(date(2021, 3, 1)), date(2021, 4, 1))
-        self.assertEqual(VakantieVerhuurVergunning.next_april_first(date(2021, 4, 1)), date(2022, 4, 1))
-        self.assertEqual(VakantieVerhuurVergunning.next_april_first(date(2021, 6, 1)), date(2022, 4, 1))
+        self.assertEqual(
+            VakantieVerhuurVergunning.next_april_first(date(2021, 3, 1)),
+            date(2021, 4, 1),
+        )
+        self.assertEqual(
+            VakantieVerhuurVergunning.next_april_first(date(2021, 4, 1)),
+            date(2022, 4, 1),
+        )
+        self.assertEqual(
+            VakantieVerhuurVergunning.next_april_first(date(2021, 6, 1)),
+            date(2022, 4, 1),
+        )
 
     def test_to_transition_agreement(self):
-        self.assertEqual(BBVergunning.to_transition_agreement('Verleend met overgangsrecht'), True)
-        self.assertEqual(BBVergunning.to_transition_agreement('Verleend zonder overgangsrecht'), False)
-        self.assertEqual(BBVergunning.to_transition_agreement('abc'), False)
+        self.assertEqual(
+            BBVergunning.to_transition_agreement("Verleend met overgangsrecht"), True
+        )
+        self.assertEqual(
+            BBVergunning.to_transition_agreement("Verleend zonder overgangsrecht"),
+            False,
+        )
+        self.assertEqual(BBVergunning.to_transition_agreement("abc"), False)
 
     def test_TVM_RVV_Object(self):
         zaak_source = {
@@ -32,14 +54,14 @@ class ZaaktypesTest(TestCase):
             "text7": "TEST MA Decos",
             "text10": "10:00",
             "date7": "2021-04-28T00:00:00",
-            "document_date": "2021-04-16T00:00:00"
+            "document_date": "2021-04-16T00:00:00",
         }
 
         zaak_transformed = {
             "caseType": "TVM - RVV - Object",
             "title": "Tijdelijke verkeersmaatregel",
             "identifier": "Z/20/1234567",
-            "dateRequest": to_date(zaak_source['document_date']),
+            "dateRequest": to_date(zaak_source["document_date"]),
             "status": "Ontvangen",
             "decision": None,
             "dateDecision": None,
@@ -48,20 +70,88 @@ class ZaaktypesTest(TestCase):
             "location": "Amstel 1 1000AB",
             "timeEnd": "16:00",
             "timeStart": "10:00",
-            "dateStart": to_date(zaak_source['date6']),
-            "dateEnd": to_date(zaak_source['date7']),
+            "dateStart": to_date(zaak_source["date6"]),
+            "dateEnd": to_date(zaak_source["date7"]),
         }
 
         self.assertEqual(TVM_RVV_Object(zaak_source).zaak, zaak_transformed)
 
-    # def test_VakantieVerhuurVergunning(self):
-    #     self.assertEqual()
+    def test_VakantieVerhuurVergunning(self):
+        zaak_source = {
+            "text45": "Vakantieverhuur vergunningsaanvraag",
+            "mark": "Z/21/7865356778",
+            "document_date": "2021-05-19T00:00:00",
+            "date5": "2021-05-19T00:00:00",
+            "text6": "Amstel 1 1012AA Amsterdam",
+            "title": "Ontvangen",
+            "dfunction": None,
+        }
+        zaak_transformed = VakantieVerhuurVergunning(zaak_source).result()
+        self.assertEqual(zaak_transformed["status"], "Afgehandeld")
+        self.assertEqual(zaak_transformed["decision"], "Verleend")
 
-    # def test_VakantieVerhuur(self):
-    #     self.assertEqual()
+        zaak_source = {
+            "text45": "Vakantieverhuur vergunningsaanvraag",
+            "mark": "Z/21/123123123",
+            "document_date": "2020-05-20T00:00:00",
+            "date5": "2020-05-20T00:00:00",
+            "text6": "Amstel 1 1012AA Amsterdam",
+            "title": "Afgehandeld",
+            "dfunction": "Ingetrokken",
+        }
+        zaak_transformed = VakantieVerhuurVergunning(zaak_source).result()
+        self.assertEqual(zaak_transformed["status"], "Afgehandeld")
+        self.assertEqual(zaak_transformed["decision"], "Ingetrokken")
 
-    # def test_VakantieVerhuurAfmelding(self):
-    #     self.assertEqual()
+    @freeze_time("2021-07-15")
+    def test_VakantieVerhuur(self):
+        zaak_source = {
+            "date6": "2021-07-05T00:00:00",
+            "date7": "2021-07-06T00:00:00",
+            "document_date": "2021-04-28T00:00:00",
+            "mark": "Z/21/67890123",
+            "subject1": "Melding Amstel 1  - V Achternaam",
+            "text11": "Nvt",
+            "text12": "Geen kosten",
+            "text45": "Vakantieverhuur",
+            "text6": "Amstel 1 1012AA Amsterdam",
+            "text7": "Z/11/123456",
+            "title": "Ontvangen",
+        }
+        zaak_transformed = VakantieVerhuur(zaak_source).result()
+
+        self.assertEqual(zaak_transformed["caseType"], "Vakantieverhuur")
+        self.assertEqual(zaak_transformed["title"], "Afgelopen verhuur")
+        self.assertEqual(zaak_transformed["decision"], None)
+        self.assertEqual(zaak_transformed["dateStart"], to_date("2021-07-05"))
+        self.assertEqual(zaak_transformed["dateEnd"], to_date("2021-07-06"))
+        self.assertEqual(zaak_transformed["dateRequest"], to_date("2021-04-28"))
+
+        zaak_source["date6"] = "2021-07-25T00:00:00"
+        zaak_source["date7"] = "2021-07-30T00:00:00"
+
+        zaak_transformed = VakantieVerhuur(zaak_source).result()
+
+        self.assertEqual(zaak_transformed["title"], "Geplande verhuur")
+        self.assertEqual(zaak_transformed["dateStart"], to_date("2021-07-25"))
+        self.assertEqual(zaak_transformed["dateEnd"], to_date("2021-07-30"))
+
+    def test_VakantieVerhuurAfmelding(self):
+        zaak_source = {
+            "company": "Moes",
+            "date6": "2021-06-18T00:00:00",
+            "date7": "2021-06-21T00:00:00",
+            "document_date": "2021-06-04T00:00:00",
+            "mark": "Z/21/89012345",
+            "subject1": "Melding Amstel 1  - R  Moes",
+            "text11": "Nvt",
+            "text45": "Vakantieverhuur afmelding",
+            "text6": "Amstel 1 1012AK",
+            "title": "Ontvangen",
+        }
+        zaak_transformed = VakantieVerhuurAfmelding(zaak_source).result()
+        self.assertEqual(zaak_transformed["title"], "Geannuleerde verhuur")
+        self.assertEqual(zaak_transformed["caseType"], "Vakantieverhuur afmelding")
 
     # def test_BBVergunning(self):
     #     self.assertEqual()
@@ -94,10 +184,13 @@ class ZaaktypesTest(TestCase):
             "text8": "KN-UW-TS",
             "text45": "Parkeerontheffingen Blauwe zone particulieren",
             "title": "Ontvangen",
-            "dfunction": "Verleend"
+            "dfunction": "Verleend",
         }
         zaak_transformed = BZP(zaak_source).result()
-        self.assertEqual(zaak_transformed["caseType"], "Parkeerontheffingen Blauwe zone particulieren")
+        self.assertEqual(
+            zaak_transformed["caseType"],
+            "Parkeerontheffingen Blauwe zone particulieren",
+        )
         self.assertEqual(zaak_transformed["kenteken"], "KN-UW-TS")
         self.assertEqual(zaak_transformed["dateStart"], to_date("2021-06-26"))
 
@@ -111,9 +204,11 @@ class ZaaktypesTest(TestCase):
             "text8": "Uw bedrijfje",
             "text45": "Parkeerontheffingen Blauwe zone bedrijven",
             "title": "Ontvangen",
-            "dfunction": "Verleend"
+            "dfunction": "Verleend",
         }
         zaak_transformed = BZB(zaak_source).result()
-        self.assertEqual(zaak_transformed["caseType"], "Parkeerontheffingen Blauwe zone bedrijven")
+        self.assertEqual(
+            zaak_transformed["caseType"], "Parkeerontheffingen Blauwe zone bedrijven"
+        )
         self.assertEqual(zaak_transformed["companyName"], "Uw bedrijfje")
         self.assertEqual(zaak_transformed["dateStart"], to_date("2021-05-26"))
