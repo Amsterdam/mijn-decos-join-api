4bf780813ceb82e0937f2b3b5ad2e59862bc9a65
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2021-10-11 16:32:00 +0200

    Integrate openapi spec validation

--- run-dev.sh
+++ run-dev.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export DECOS_JOIN_ADRES_BOEKEN_BSN="foo,bar"
+export DECOS_JOIN_ADRES_BOEKEN_KVK="bar,foo"
 export FLASK_ENV=development
 export FLASK_APP=./decosjoin/server.py
 
