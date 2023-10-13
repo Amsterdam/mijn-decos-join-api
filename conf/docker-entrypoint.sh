fbe11f8fc25c0eb2e62ce8abb8a4b608b35a4b26
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2023-10-13 10:59:10 +0200

    Add condition

--- conf/docker-entrypoint.sh
+++ conf/docker-entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
-# echo "Starting SSH ..."
+# AZ AppService allows SSH into a App instance.
+if [ $MA_OTAP_ENV == "test" ]
+then
+ # echo "Starting SSH ..."
 service ssh start
+fi
 
 uwsgi --uid www-data --gid www-data --ini /api/uwsgi.ini
