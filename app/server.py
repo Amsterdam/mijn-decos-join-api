afe95bd258c0480a867f0466bf4814a7dce6e848
Thomas Vixseboxse <thmsv@Thomass-MacBook-Pro-2.local>
2024-04-08 16:49:09 +0200

    fix ***

--- app/server.py
+++ app/server.py
@@ -10,7 +10,7 @@ from azure.monitor.opentelemetry import configure_azure_monitor
 from opentelemetry import trace
 
 configure_azure_monitor(
-    connection_string = os.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING"))
+    connection_string=os.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING"))
 
 from app import auth
 from app.config import IS_AZ, IS_DEV, SENTRY_ENV, UpdatedJSONProvider, get_sentry_dsn
